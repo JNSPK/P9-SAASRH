3e5eb7b2a67f0cb89cfcb36a8e47d83bbed14860
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-08-29 18:41:08 +0200

    fix(dashboard):can't be stuck in navigation anymore

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -95,7 +95,13 @@ export default class {
   };
 
   handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0;
+    console.log(this.counter);
+    if (
+      this.counter === undefined ||
+      (this.counter % 2 !== 0 && this.counter !== 1) ||
+      this.id !== bill.id
+    )
+      this.counter = 0;
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id;
     if (this.counter % 2 === 0) {
       bills.forEach((b) => {
@@ -106,12 +112,10 @@ export default class {
       $('.vertical-navbar').css({ height: '150vh' });
       this.counter++;
     } else {
-      $(`#open-bill${bill.id}`).css({ background: '#2A2B35' });
-
-      // $('.dashboard-right-container div').html(`
-      //   <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-      // `);
-      $('.dashboard-right-container div').html(DashboardFormUI(bill));
+      $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' });
+      $('.dashboard-right-container div').html(`
+        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+      `);
       $('.vertical-navbar').css({ height: '120vh' });
       this.counter++;
     }
@@ -141,35 +145,22 @@ export default class {
   };
 
   handleShowTickets(e, bills, index) {
-    if (this.index === undefined) {
-      this.index = index;
-      this.counter = 0;
-    }
-
-    if (this.index !== index) {
-      this.index = index;
-      this.counter = 1;
-    } else {
-      this.counter++;
-    }
-
+    if (this.counter === undefined || this.index !== index) this.counter = 0;
+    if (this.index === undefined || this.index !== index) this.index = index;
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' });
       $(`#status-bills-container${this.index}`).html(
         cards(filteredBills(bills, getStatus(this.index)))
       );
+      this.counter++;
     } else {
+      $('.dashboard-right-container div').html(`
+        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+      `);
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' });
       $(`#status-bills-container${this.index}`).html('');
+      this.counter++;
     }
-    //  {
-    //   $('.dashboard-right-container div').html(`
-    //     <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-    //   `);
-    //   bills.forEach((b) => {
-    //     $(`#open-bill${b.id}`).css({ background: '#0D5AE5' });
-    //   });
-    // }
 
     bills.forEach((bill) => {
       $(`#open-bill${bill.id}`).click((e) =>
